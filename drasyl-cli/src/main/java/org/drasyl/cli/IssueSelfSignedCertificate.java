5e7bf6e62c414514cb777566a7e039826a9b696c
finn <finn.eilmann@web.de>
2025-01-08 18:48:18 +0100

    copyright has to be exact (and is outdated)

--- drasyl-cli/src/main/java/org/drasyl/cli/IssueSelfSignedCertificate.java
+++ drasyl-cli/src/main/java/org/drasyl/cli/IssueSelfSignedCertificate.java
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2024 Finn Eilmann
+ * Copyright (c) 2020-2024 Heiko Bornholdt and Kevin Röbert
  *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
